d92d6dc0b83bbc9c276cd563c6169db9bcc25a6f
Andrew Augustine <me@azureagst.dev>
2026-05-11 18:36:26 -0400

    Add initial Models

---
+++ DocumizeConnector/Models/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumizeConnector.Models
+{
+    internal class Document
+    {
+        [Key]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public List<string> tags { get; set; }
+    }
+}
